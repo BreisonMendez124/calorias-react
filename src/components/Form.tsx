a159483b3c57d8a1e56482249c38cd4de3a21323
Breison Mendez <Mendez12361@outlook.com>
2024-03-21 20:58:55 -0500

    Convertir campos de texto a numericos Los campos de Categoria y Calorias se parsean a numericos Se crea el type o interfaz del state activity

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,20 @@
 import { useState, ChangeEvent } from "react";
 import { categories } from "../data/categories"
+import { Activity } from "../types/categories";
 
 function Form() {
 
-    const [ activity , setActivity ] = useState({
+    const [ activity , setActivity ] = useState<Activity>({
         category: 0 ,
         nameActivity: '',
         calories: 0
     });
 
     const handleChange = ( e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>  ) => {
+        const isNumberField = ['category' , 'calories'].includes( e.target.id );
         setActivity( { 
             ...activity,
-            [ e.target.id ]: e.target.value
+            [ e.target.id ]: isNumberField ? +e.target.value : e.target.value
         })
     }
 
--- src/types/categories.ts
+++ src/types/categories.ts
@@ -2,3 +2,9 @@ export type Categories = {
     id: number;
     name: string;
 }
+
+export type Activity = {
+    category: number;
+    nameActivity: string;
+    calories: number;
+}
