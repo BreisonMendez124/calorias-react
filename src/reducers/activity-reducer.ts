692af8a026f745b142c3caf55f709e3c9fe0de6c
Breison Mendez <Mendez12361@outlook.com>
2024-03-26 17:02:10 -0500

    Agregar propiedades al reducer Se agrega la propiedad set-activeId para guardar el id de la actividad seleccionada y se termina la logica de guardado sin edicion

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,14 +1,17 @@
 import { Activity } from "../types"
 
 export type ActivityActions = 
-    { type: 'save-activity', payload: { newActivity: Activity } }
+    { type: 'save-activity', payload: { newActivity: Activity } } | 
+    { type: 'set-activeId', payload: { id: Activity['id'] } }
 
-type ActivityState = { 
-    activities: Activity[]
+export type ActivityState = { 
+    activities: Activity[],
+    activeId: Activity['id']
 }
 
 export const initialState: ActivityState = {
-    activities: []
+    activities: [],
+    activeId:''
 }
 
 export const activityReducer = ( 
@@ -17,6 +20,17 @@ export const activityReducer = (
 ) => {
     if( action.type === 'save-activity'){ 
         console.log('Desde el type de save-activity');
+        return { 
+            ...state,
+            activities: [ ...state.activities, action.payload.newActivity ]
+        }
+    }
+
+    if( action.type === 'set-activeId'){ 
+        return { 
+            ...state,
+            activeId: action.payload.id
+        }
     }
 
     return state
