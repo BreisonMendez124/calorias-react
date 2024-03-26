02e638e9994eaa9d30884a4a96dcf82ed5a63b96
Breison Mendez <Mendez12361@outlook.com>
2024-03-26 17:03:24 -0500

    Referenciar el state del reducer y insertar el componente de ActivityLi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Form from "./components/Form"
 import { useReducer} from "react";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
+import ActivityList from "./components/ActivityList";
 function App() {
   const [ state , dispatch ] = useReducer( activityReducer, initialState );
   return (
@@ -17,9 +18,16 @@ function App() {
         <div className="max-w-4xl mx-auto">
           <Form
               dispatch={ dispatch }
+              state={ state }
             />
         </div>
      </section>
+
+     <div className="p-10 mx-auto max-w-4xl">
+        <ActivityList 
+                activities={ state.activities }
+                dispatch={ dispatch } />
+     </div>
     </>
   )
 }
